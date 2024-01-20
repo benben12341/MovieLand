b12aded398120a05ccaf45463ec4a9ca85fe78ad
Dror <drormizrahi01@gmail.com>
2024-01-20 02:10:07 +0200

    Add Movie Search Bar

---
+++ movie-land/frontend/src/components/MovieFilter.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import TextField from '@mui/material/TextField';
+import InputAdornment from '@mui/material/InputAdornment';
+import SearchIcon from '@mui/icons-material/Search';
+
+const MovieFilter = ({ onFilterChange }) => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSearchChange = (event) => {
+    const newSearchTerm = event.target.value;
+    setSearchTerm(newSearchTerm);
+    onFilterChange(newSearchTerm);
+  };
+
+  return (
+    <TextField
+      fullWidth
+      variant="outlined"
+      margin="normal"
+      label="Search Movies"
+      value={searchTerm}
+      onChange={handleSearchChange}
+      InputProps={{
+        startAdornment: (
+          <InputAdornment position="start">
+            <SearchIcon />
+          </InputAdornment>
+        ),
+      }}
+    />
+  );
+};
+
+export default MovieFilter;
--- movie-land/frontend/src/pages/MovieList/index.jsx
+++ movie-land/frontend/src/pages/MovieList/index.jsx
@@ -1,3 +1,48 @@
-const MovieList = () => <div></div>;
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { listMovies } from "../../actions/movieActions";
+import Movie from "../../components/Movie";
+import CircularProgress from "@mui/material/CircularProgress";
+import { Box } from "@mui/material";
+import MovieFilter from "../../components/MovieFilter";
+
+const MovieList = () => {
+  const dispatch = useDispatch();
+  const movieList = useSelector((state) => state.movieList);
+  const [filteredMovies, setFilteredMovies] = useState([]);
+  const { loading, error, movies } = movieList;
+
+  useEffect(() => {
+    dispatch(listMovies(""));
+  }, [dispatch]);
+
+  useEffect(() => {
+    setFilteredMovies(movieList.movies);
+  }, [movieList]);
+
+  const handleFilterChange = (searchTerm) => {
+    const filtered =
+      searchTerm === ""
+        ? movies
+        : movies.filter((movie) =>
+            movie.name.toLowerCase().includes(searchTerm.toLowerCase())
+          );
+
+    setFilteredMovies(filtered);
+  };
+
+  return (
+    <>
+      <MovieFilter onFilterChange={handleFilterChange} />
+      {loading ? (
+        <Box sx={{ display: "flex" }}>
+          <CircularProgress />
+        </Box>
+      ) : (
+        filteredMovies.map((movie, index) => <Movie movie={movie} />)
+      )}
+    </>
+  );
+};
 
 export default MovieList;
