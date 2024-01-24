2f660942b36d9392a0fc7f1d7296e676e67ae16a
Adi Ben Shushan <adi3765@gmail.com>
2024-01-25 00:21:28 +0200

    Delete unnecessary function

--- movie-land/frontend/src/actions/userActions.js
+++ movie-land/frontend/src/actions/userActions.js
@@ -219,6 +219,7 @@ export const updateUserProfile = (user) => async (dispatch, getState) => {
   }
 };
 
+
 export const listUsers = () => async (dispatch, getState) => {
   try {
     dispatch({
@@ -276,43 +277,6 @@ export const deleteUser = (id) => async (dispatch, getState) => {
   } catch (error) {
     dispatch({
       type: USER_DELETE_FAIL,
-      payload:
-        error.response && error.response.data.message
-          ? error.response.data.message
-          : error.message,
-    });
-  }
-};
-
-export const updateUser = (user) => async (dispatch, getState) => {
-  try {
-    dispatch({
-      type: USER_UPDATE_REQUEST,
-    });
-
-    const {
-      userLogin: { userInfo },
-    } = getState();
-
-    const config = {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${userInfo.token}`,
-      },
-    };
-
-    const { data } = await axios.put(`/api/users/${user._id}`, user, config);
-
-    dispatch({
-      type: USER_UPDATE_SUCCESS,
-    });
-    dispatch({
-      type: USER_DETAILS_SUCCESS,
-      payload: data,
-    });
-  } catch (error) {
-    dispatch({
-      type: USER_UPDATE_FAIL,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
